e686a0945f06f6bffea9fca1c5dccb95c4e859f2
koubouhirx7-cloud <koubou.hi.rx7@gmail.com>
2026-04-15 14:01:06 +0900

    fix: include all HTML in vercel bundle and add PUT to CORS

--- backend/main.py
+++ backend/main.py
@@ -74,7 +74,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Content-Type", "Authorization"],
 )
 
